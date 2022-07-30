a9903295f085dd04db3f61f063dd98eb9baf5121
ArrthurGeronimo <artha.desenvolvimento@gmail.com>
2022-07-30 16:23:28 -0300

    Next + Chakra UI

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <div>
+      <Alert status="error">
+        <AlertIcon />
+        <AlertTitle>Your browser is outdated!</AlertTitle>
+        <AlertDescription>
+          Your Chakra experience may be degraded.
+        </AlertDescription>
+      </Alert>
+    </div>
+  );
+}
